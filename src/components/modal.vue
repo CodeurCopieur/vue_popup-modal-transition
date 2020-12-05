542f0a5bf36c31bb51cdf0b8a2910765d542a1b5
codwerk <louis_widdy@live.fr>
2020-12-05 11:42:20 +0100

    component modal

---
+++ src/components/modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="modal-overlay">
+    <h1>modal</h1>
+  </div>
+</template>
+
+<script>
+ export default {
+   data() {
+     return {}
+   }
+  }
+</script>
+
+<style>
+  
+.modal-overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 98;
+  background-color: rgba(0, 0, 0, .3);
+}
+</style>
