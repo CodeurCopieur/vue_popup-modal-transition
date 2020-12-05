532500bc645e5e9a615b6af5626130aa988603d5
codwerk <louis_widdy@live.fr>
2020-12-05 11:43:31 +0100

    intégration html/css, add(variable, condition)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="btn-modal" @click="showModal = !showModal">Show Modal</button>
+
+    <transition name="fade" appear>
+      <Modal v-if="showModal" @click="showModal = !showModal" />
+    </transition>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      showModal: false
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+*, ::before, ::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  min-height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: relative;
+
+  .btn-modal {
+   cursor: pointer;
+   appearance: none;
+   outline: none;
+   border: none;
+   background: none;
+   background: linear-gradient(90deg, #3bb2b8 0%, #3bb2b8 25%, #43e695 100%);
+   
+   padding: 15px 25px;
+   border-radius: 8px;
+   color: #fff;
+
+   font-size: 18px;
+   font-weight: 700;
+
+   box-shadow: 3px 3px rgba(0, 0, 0, .4);
+   transition: .4s ease-out;
+
+   &:hover {
+     box-shadow: 6px 6px rgba(0, 0, 0, .6);
+   }
+ }
 }
+
 </style>
